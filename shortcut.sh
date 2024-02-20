b90fccd96dc8451c59498ddbca0287f56b05e075
Tymoteusz <tymoteusz.lango@gmail.com>
2024-02-20 17:51:53 +0100

    Refactor

--- shortcut.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-active_window_id=$(printf 0x%x "$(xdotool getactivewindow)")
-window_state=$(xprop -id "$active_window_id" | grep "_NET_WM_STATE(ATOM)")
-
-if [[ "$window_state" == *"MAXIMIZED"* ]]; then
-    wmctrl -ir "$active_window_id" -b remove,maximized_vert,maximized_horz
-else
-    xdotool windowminimize "$active_window_id"
-fi
